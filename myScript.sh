b9b32c37b45c2670942c424ed83cb9ffcc2172d4
Vojtěch Kijenský <kijensky@gmail.com>
2022-06-02 14:53:15 +0200

    add: dotnet sdk 6.0

--- myScript.sh
+++ myScript.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install -y docker.io
-
-sudo apt-get update
 sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
@@ -12,3 +9,15 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-get update
 sudo apt-get install azure-cli    
+
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y dotnet-sdk-6.0
+sudo apt-get install -y aspnetcore-runtime-6.0
+
+sudo apt-get update
+sudo apt install -y docker.io

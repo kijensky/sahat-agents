9f65dbe40ed706be60297f807f75dd2e6fc9df9f
Vojtěch Kijenský <kijensky@gmail.com>
2022-06-02 15:43:43 +0200

    add kubectl

--- myScript.sh
+++ myScript.sh
@@ -23,3 +23,6 @@ sudo apt-get update
 sudo apt install -y docker.io
 
 sudo apt-get install -y npm
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
